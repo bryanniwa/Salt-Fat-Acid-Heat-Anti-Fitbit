1f56e66e0a485aa6912ebccc85fb5f161c810f88
Bryan Niwa <bryan_niwa@sfu.ca>
2021-02-13 14:59:40 -0800

    Create FoodLog activity

--- app/src/main/java/ca/thebois/saltfatacidheat_anti_fitbit/MainActivity.java
+++ app/src/main/java/ca/thebois/saltfatacidheat_anti_fitbit/MainActivity.java
@@ -1,19 +1,17 @@
 package ca.thebois.saltfatacidheat_anti_fitbit;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,18 +23,14 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        fab.setOnClickListener(view ->
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+                .setAction("Action", null).show());
 
         Button button = findViewById(R.id.test_btn);
-        button.setOnClickListener((listener) -> {
-            Toast.makeText(this, "you clicked me!", Toast.LENGTH_SHORT)
-                .show();
+        button.setOnClickListener(v -> {
+            Intent i = FoodLogActivity.makeLaunchIntent(this);
+            startActivity(i);
         });
     }
 
