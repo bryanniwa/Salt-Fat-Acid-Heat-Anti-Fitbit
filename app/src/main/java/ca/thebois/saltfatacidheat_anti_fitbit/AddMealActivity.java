7937f05f2620b55f6f5600ea8862b99a2a8968b0
Bryan Niwa <bryan_niwa@sfu.ca>
2021-02-13 15:28:19 -0800

    Create activity to add a new meal
    Create a basic activity and wire it to the FAB on the main activity

---
+++ app/src/main/java/ca/thebois/saltfatacidheat_anti_fitbit/AddMealActivity.java
@@ -0,0 +1,34 @@
+package ca.thebois.saltfatacidheat_anti_fitbit;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class AddMealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_meal);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static Intent makeLaunchIntent(Context context) {
+        return new Intent(context, AddMealActivity.class);
+    }
+}
