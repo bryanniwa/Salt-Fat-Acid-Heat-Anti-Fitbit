2a6b18ea36f356aeba2536db18e20965a15657a4
Bryan Niwa <bryan_niwa@sfu.ca>
2021-02-14 08:37:02 -0800

    Ask user for activity permission

--- app/src/main/java/ca/thebois/saltfatacidheat_anti_fitbit/MainActivity.java
+++ app/src/main/java/ca/thebois/saltfatacidheat_anti_fitbit/MainActivity.java
@@ -1,20 +1,47 @@
 package ca.thebois.saltfatacidheat_anti_fitbit;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.location.ActivityTransition;
+import com.google.android.gms.location.ActivityTransitionEvent;
+import com.google.android.gms.location.ActivityTransitionResult;
+import com.google.android.gms.location.DetectedActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements
+        ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private final boolean runningQOrLater =
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q;
+    private boolean activityTrackingEnabled;
+    private List<ActivityTransition> activityTransitionList;
+    private final String TRANSITIONS_RECEIVER_ACTION =
+            BuildConfig.APPLICATION_ID + "TRANSITIONS_RECEIVER_ACTION";
+    private PendingIntent activityTransitionPendingIntent;
+    private TransitionsReceiver transitionsReceiver;
+    private final int PERMISSION_REQUEST_ACTIVITY_RECOGNITION = 45;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +61,56 @@ public class MainActivity extends AppCompatActivity {
             Intent i = FoodLogActivity.makeLaunchIntent(this);
             startActivity(i);
         });
+
+        activityTrackingEnabled = false;
+
+        populateTransitionList();
+        Intent intent = new Intent(TRANSITIONS_RECEIVER_ACTION);
+        activityTransitionPendingIntent =
+                PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
+
+        transitionsReceiver = new TransitionsReceiver();
+
+        getPermissionsIfNotGranted();
+    }
+
+    private void getPermissionsIfNotGranted() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACTIVITY_RECOGNITION},
+                    PERMISSION_REQUEST_ACTIVITY_RECOGNITION);
+        }
+    }
+
+    private void populateTransitionList() {
+        activityTransitionList = new ArrayList<>();
+        activityTransitionList.add(new ActivityTransition.Builder()
+                .setActivityType(DetectedActivity.WALKING)
+                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
+                .build()
+        );
+        activityTransitionList.add(new ActivityTransition.Builder()
+                .setActivityType(DetectedActivity.WALKING)
+                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
+                .build()
+        );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        registerReceiver(transitionsReceiver, new IntentFilter(TRANSITIONS_RECEIVER_ACTION));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_ACTIVITY_RECOGNITION) {
+            if (activityRecognitionPermissionApproved()) {
+                activityTrackingEnabled = true;
+            }
+        }
     }
 
     @Override
@@ -56,5 +133,28 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private boolean activityRecognitionPermissionApproved() {
+        if (runningQOrLater) {
+            return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACTIVITY_RECOGNITION
+            );
+        } else {
+            return true;
+        }
+    }
+
+    public class TransitionsReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if (ActivityTransitionResult.hasResult(intent)) {
+                ActivityTransitionResult result = ActivityTransitionResult.extractResult(intent);
+
+            }
+        }
     }
 }
